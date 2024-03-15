fdfa6d6435a9c0b4adeee9f38dfd2efa75f195a4
gautam arashi <gautamarashi8877@gmail.com>
2024-03-15 10:28:05 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -15,28 +15,28 @@ function App() {
   return (
    <>
    <div className="flex">
-    <div className="min-w-[200px] ">
-      <div className="bg-red-300 p-2 w-[350px] h-[140px] m-2 rounded-md">
-      <div className="m-5 ">Choose category</div>
+    <div className="min-w-[200px z-10] ">
+      <div className="bg-red-300 p-2 w-[350px] h-full m-2 rounded-md">
+      <div className="m-5 ">●Choose Categories</div>
       <select name="" id="" className='w-60 border-2 ml-10 border-black'>
-        <option value="">men's clothing</option>
-        <option value="">jewelery</option>
-        <option value=""> electronics</option>
-        <option value="">women's clothing</option>
+        <option value="">Men's Clothes</option>
+        <option value="">jewellery</option>
+        <option value=""> Electronics</option>
+        <option value="">Women's clothing</option>
        
       </select>
       </div>
      
     </div>
-    <div className="center gap-10 flex-wrap">
+    <div className="center gap-10 flex-wrap bg-gray-200 to bg-gray-200 ">
 
 {data.map ((item)=>{
   return(
-    <div className="max-w-[400px] h-[500px] bg-gray-300  rounded-2xl p-2">
-      <div className="bg-red-500 center"><img src={item.image} alt="" className='h-[290px] w-[390px]'/></div>
-      <div className="text-xl">{item.title}</div>
-      <div className="">category : {item.category}</div>
-      <div className="">Price: ${item.price}</div>
+    <div className="max-w-[400px] h-[500px] bg-orange-300 m-2 hover:bg-orange-500  rounded-2xl p-2 cursor-pointer">
+      <div className="bg-red-500 "><img src={item.image} alt="" className='h-[290px] w-[390px]'/></div>
+      <div className="h-6 font-bold l  text-xl mb-8 ">●{item.title}</div>
+      <div className="h-6 font-bold   text-xl mb-5 "> ●Categories: {item.category}</div>
+      <div className="h-6 font-bold   text-xl "> ●Price: ${item.price}</div>
     </div>
   )
 })}
